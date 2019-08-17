ae94e87e95af862d875400df63ea7d369d03bb02
Francisco Navarrete <francisco.navarrete@walmart.com>
2019-08-17 17:28:05 -0500

    Added all classes

---
+++ src/main/java/mx/com/devs4j/microservices/WebSecurityConfigurer.java
@@ -0,0 +1,29 @@
+package mx.com.devs4j.microservices;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	@Bean
+	public UserDetailsService userDetailsServiceBean() throws Exception {
+		return super.userDetailsServiceBean();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("francisco.navarrete").password("{noop}password1").roles("USER").and()
+				.withUser("alberto.barros").password("{noop}password2").roles("USER", "ADMIN");
+	}
+}
